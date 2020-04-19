774f4519c682589a25efa39b5fc622efb6a5321e
mnejus <nejman.michal@gmail.com>
2020-04-19 16:48:16 +0200

    a

--- blog/models.py
+++ blog/models.py
@@ -1,13 +1,19 @@
 from django.db import models
 from django.utils import timezone
 
+class Category(models.Model):
+	name = models.CharField(max_length=200, null=True)
+
+	def __str__(self):
+		return self.name
+
 class Post(models.Model):
 	author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
 	title = models.CharField(max_length=200)
 	text = models.TextField()
 	created_data = models.DateTimeField(default=timezone.now)
 	published_date = models.DateTimeField(blank=True, null=True)
-	category = models.ManyToManyField(Category, verbose_name='Kategoria', on_delete=models.SET_NULL, null=True)
+	category = models.ManyToManyField(Category, verbose_name='Kategoria', help_text='Wybierz kategorię wpisu')
 	
 	def publish(self):
 		self.published_date = timezone.now()
@@ -15,9 +21,3 @@ class Post(models.Model):
 		
 	def __str__(self):
 		return self.title
-
-class Category(models.Model):
-	name = models.CharField(max_length=200, null=True)
-
-	def __str__(self):
-		return self.name
