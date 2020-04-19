41a0152d458bd7391214bb84965c19d72155325b
mnejus <nejman.michal@gmail.com>
2020-04-19 21:22:47 +0200

    zmiana kol wyswietlania

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,7 @@ from .forms import PostForm
 from django.contrib.auth.decorators import login_required
 
 def post_list(request):
-	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
 	categories = Category.objects.all()
 
 	context = {
